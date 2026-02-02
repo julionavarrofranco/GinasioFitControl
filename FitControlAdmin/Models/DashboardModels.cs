7c8f2d399f11647ed0559682b8bade868417b89a
DiSyllar <tiago.pinto25@hotmail.com>
2026-02-02 22:26:32 +0000

    Remoção de ficheiros desnecessários

--- FitControlAdmin/Models/DashboardModels.cs
+++
@@ -1,9 +0,0 @@
-namespace FitControlAdmin.Models
-{
-    public class DashboardSummaryDto
-    {
-        public int MembrosAtivos { get; set; }
-        public int PTsAtivos { get; set; }
-        public decimal ReceitaMensal { get; set; }
-    }
-}
--- FitControlAdmin/Models/UpdateEmployeeDto.cs
+++
@@ -1,9 +0,0 @@
-namespace FitControlAdmin.Models
-{
-    public class UpdateEmployeeDto
-    {
-        public string? Nome { get; set; }
-        public string? Telemovel { get; set; }
-        public string? Funcao { get; set; }
-    }
-}
